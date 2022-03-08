92b29580544c28b9ceb28d47c687e31d3e079e9d
Adekunle Ismail <adismail4@gmail.com>
2022-03-09 00:01:45 +0100

    Add book component

--- src/pages/bookPage.js
+++ src/pages/bookPage.js
@@ -1,69 +1,17 @@
 import React from "react";
-import Form from "../components/form";
-
+import Book from "../components/book";
 import Header from "../components/header";
 
 const bookPage = () => (
   <main>
     <Header />
-    <div className="card-container">
-      <p className="book-category">Action</p>
-      <h3>The Hunger Games</h3>
-      <p>Suzanne Collins</p>
-      <p>Comments</p>
-      <span />
-      <p>Remove</p>
-      <span />
-      <p>Edit</p>
-      <span />
-      <div className="progress-circle">
-        <span>64%</span>
-        <p>Completed</p>
-      </div>
-      <hr />
-      <p>CURRENT CHAPTER</p>
-      <p>Chapter 17</p>
-      <button type="submit">UPDATE PROGRESS</button>
-    </div>
-    <div className="card-container">
-      <p className="book-category">Science Fiction</p>
-      <h3>Dune</h3>
-      <p>Frank Herbert</p>
-      <p>Comments</p>
-      <span />
-      <p>Remove</p>
-      <span />
-      <p>Edit</p>
-      <span />
-      <div className="progress-circle">
-        <span>8%</span>
-        <p>Completed</p>
-      </div>
-      <hr />
-      <p>CURRENT CHAPTER</p>
-      <p>Chapter 3: A Lesson Learned</p>
-      <button type="submit">UPDATE PROGRESS</button>
-    </div>
-    <div className="card-container">
-      <p className="book-category">Action</p>
-      <h3>Capital in the Twenty-First Century</h3>
-      <p>Suzanne Collins</p>
-      <p>Comments</p>
-      <span />
-      <p>Remove</p>
-      <span />
-      <p>Edit</p>
-      <span />
-      <div className="progress-circle">
-        <span>0%</span>
-        <p>Completed</p>
-      </div>
-      <hr />
-      <p>CURRENT CHAPTER</p>
-      <p>Introduction</p>
-      <button type="submit">UPDATE PROGRESS</button>
-    </div>
-    <Form />
+    <Book category="Action" author="Suzanne Collins" title="The Hunger Games" />
+    <Book category="Science Fiction" author="Frank Herbert" title="Dune" />
+    <Book
+      category="Economy"
+      author="Suzanne Collins"
+      title="Capital in the Twenty-First Century"
+    />
   </main>
 );
 
